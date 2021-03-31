4a86fd02864dc9dfc32136f553955be38d5d77ef
Michael Tobias <axis139@gmail.com>
2021-03-30 19:36:09 -0700

    add styling packages

---
+++ styles/theme.ts
@@ -0,0 +1,13 @@
+import { Theme } from "@emotion/react"
+
+const theme: Theme = {
+  darkMode: true,
+
+  color: {
+    primary: '#000',
+    positive: '#fff',
+    negative: '#000'
+  },
+}
+
+export default theme
