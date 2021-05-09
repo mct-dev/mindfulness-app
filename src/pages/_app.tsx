b3dd62799159651a97c2f69911c7013afdb51081
Michael Tobias <axis139@gmail.com>
2021-05-09 15:00:35 -0700

    add firebase import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@emotion/react'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { useEffect } from 'react'
 import { createGlobalStyles, Global, theme } from '~styles'
+import '~util/firebase'
 
 function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
   useEffect(() => {
